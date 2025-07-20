58e706e359de6e8c339911318c2c8dd15eb75ac5
Ominitriix <paixaoca2003@gmail.com>
2025-07-20 14:48:21 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Entregadora-Template
+Desenvolvido um template de uma entregadora de encomendas de pequeno e médio porte com ideias de innovação na área.
---
+++ dist/scrollreveal.js
@@ -0,0 +1,61 @@
+ScrollReveal().reveal(".logo", {
+  duration: 1000,
+  opacity: 0,
+});
+
+ScrollReveal().reveal(".input-slide", {
+  duration: 1000,
+  opacity: 0,
+  delay: 300,
+  origin: "right",
+  distance: "1rem",
+});
+
+ScrollReveal().reveal(".navbar-social li", {
+  duration: 1000,
+  delay: 600,
+  interval: 600,
+  opacity: 0,
+  origin: "right",
+  distance: "1rem",
+});
+
+ScrollReveal().reveal(".title", {
+  duration: 1000,
+  delay: 300,
+  opacity: 0,
+});
+
+ScrollReveal().reveal(".prg", {
+  duration: 1000,
+  delay: 600,
+  opacity: 0,
+});
+
+ScrollReveal().reveal(".line-vert-slide", {
+  duration: 1000,
+  opacity: 0,
+  origin: "left",
+  distance: "1rem",
+});
+
+ScrollReveal().reveal(".img-slide", {
+  duration: 1000,
+  delay: 1200,
+  opacity: 0,
+  distance: "1rem",
+  origin: "left",
+});
+
+ScrollReveal().reveal(".img-slide-top", {
+  duration: 1000,
+  opacity: 0,
+  distance: "1rem",
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".flex-buttons", {
+  duration: 1000,
+  delay: 900,
+  opacity: 0,
+});
